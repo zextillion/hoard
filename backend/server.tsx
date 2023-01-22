c6e80c4ee0126f42e95b5e885e6bad4b716c1253
Zextillion <LookASupernova@gmail.com>
2023-01-21 16:05:48 -0800

    Updated CORS on server

--- backend/server.tsx
+++ backend/server.tsx
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.text())
 app.options('*', cors())
 app.use(cors({
-    origin: "https://hoard-api.onrender.com",
+    origin: ["https://hoard.onrender.com", "http://localhost:3000"],
     credentials: true,
 }))
 app.use('/igdb', igdbRouter)
